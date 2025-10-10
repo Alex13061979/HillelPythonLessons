3a9f824eda21ce222a3a1c5b9201def4a367136f
Oleksandr <alexandr.kherovimchuk@gmail.com>
2025-10-10 17:06:29 +0300

    Home work 3.1

---
+++ Lesson_3_1.py
@@ -0,0 +1,17 @@
+a = int(input("Enter any number, please: "))
+b = int(input("Enter any number, please: "))
+c = int(input("Enter any number, please: "))
+d = int(input("Enter any number, please: "))
+
+print(a, b, c, d)
+y = ((d * c + a)-b)
+print("The result of calculation is: ", y )
+
+if a != 0:
+    x = b/a
+    print("The result of divison is: ", x)
+else: print("You cannot divide to zero!")
+print("Have a nice day!")
+
+
+
