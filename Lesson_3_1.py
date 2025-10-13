eacf4b2b5c857d9c8ed3a1eaf17a5ef41f86b265
Oleksandr <alexandr.kherovimchuk@gmail.com>
2025-10-13 21:30:05 +0300

    Home work 3.1 adjusted

--- Lesson_3_1.py
+++ Lesson_3_1.py
@@ -1,17 +1,21 @@
-a = int(input("Enter any number, please: "))
-b = int(input("Enter any number, please: "))
-c = int(input("Enter any number, please: "))
-d = int(input("Enter any number, please: "))
-
-print(a, b, c, d)
-y = ((d * c + a)-b)
-print("The result of calculation is: ", y )
-
-if a != 0:
-    x = b/a
-    print("The result of divison is: ", x)
-else: print("You cannot divide to zero!")
-print("Have a nice day!")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter another number: "))
+operator = input("Enter the operator (+, -, *, /): ")
 
 
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error: Division by zero!"
+else:
+    result = "Unknown operator"
+
+print("The result:", result)
 
